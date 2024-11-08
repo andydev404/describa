81119e4c2979615978a8daae3d680ddb9effb020
Andy Santana <andysantana0106@gmail.com>
2024-11-07 20:58:36 -0400

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import dynamic from 'next/dynamic'
 import NextTopLoader from 'nextjs-toploader'
-
 import { Providers } from './providers'
-
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import '@/styles/globals.scss'
 
 export const metadata: Metadata = {
   title: 'Describa | Transform Product Descriptions with AI-Powered Precision',
   description:
-    "Boost your e-commerce sales with Describa's AI-generated product descriptions. Save time, improve SEO, and drive more conversions. Try it now!",
+    'Boost your e-commerce sales with Describa\'s AI-generated product descriptions. Save time, improve SEO, and drive more conversions. Try it now!',
   robots: {
     index: true,
     follow: true,
@@ -35,7 +34,7 @@ export const metadata: Metadata = {
     title:
       'Transform Product Descriptions with AI-Powered Precision | Describa',
     description:
-      "Boost your e-commerce sales with Describa's AI-generated product descriptions. Save time, improve SEO, and drive more conversions. Try it now!",
+      'Boost your e-commerce sales with Describa\'s AI-generated product descriptions. Save time, improve SEO, and drive more conversions. Try it now!',
     images: [
       {
         url: 'https://utfs.io/f/gzOTTZHX3WMAMUjNjNzQVO18afWMlYdDK7b5FuPkHhwRIpAX',
@@ -48,34 +47,35 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children
-}: Readonly<{
+                                     children
+                                   }: Readonly<{
   children: React.ReactNode
 }>) {
   const Crisp = dynamic(() => import('../lib/crisp'))
   return (
     <ClerkProvider>
       <html lang="en">
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <meta name="apple-mobile-web-app-title" content="Describa" />
-        <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="Describa" />
+      <link rel="manifest" href="/site.webmanifest" />
 
-        <body className={`min-h-screen font-sans text-foreground antialiased`}>
-          <NextTopLoader color={'#000000'} />
-          <Providers>{children}</Providers>
-        </body>
+      <body className={`min-h-screen font-sans text-foreground antialiased`}>
+      <NextTopLoader color={'#000000'} />
+      <Providers>{children}</Providers>
+      <Analytics />
+      </body>
       </html>
     </ClerkProvider>
   )
